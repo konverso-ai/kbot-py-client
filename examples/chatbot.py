936d8c4ecaa7803e7f4fd1b9af1de93cba8bbf2d
konverso-fpialoux <florian.pialoux@konverso.ai>
2025-02-07 07:00:22 +0100

    docs: various fixes to readme

---
+++ examples/chatbot.py
@@ -0,0 +1,24 @@
+import time
+
+from kbot_client import Client
+from kbot_client.chat_client import AsyncChatClient, SyncChatClient
+
+user_email = 'amedee.potier@konverso.ai'
+user_firstname = "Amédée"
+user_lastname = "Potier"
+
+# Create a session using an API key with strong privileges
+cli = Client("my-kbot.konverso.ai", api_key="cb47-xxxxxxxxxxxxxxxxxxxxxxxxx")
+
+response = cli.post("user/lookup_create", data={
+    "user_name": user_email,
+    "account_name": user_email,
+    "account_type": "local",
+    "external_auth": "my-app",
+})
+response.raise_for_status()
+
+cli.impersonate(user_email, 'local', 'nice-in-connect', application_uuid="b0d91200-833f-466c-b5fd-1b0402cc3a36")
+
+SyncChatClient(cli)
+
